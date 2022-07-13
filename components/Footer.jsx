5abacf9709e05bff8c8e7a81cdaf87d82596858a
Thanathip Ch <thanathip.oat@g.swu.ac.th>
2022-07-14 03:01:58 +0700

    add Footer sticky bottom

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <>
-    <footer className="p-5 text-center bg-slate-500">
+    <footer className="p-3 text-center bg-slate-500 fixed inset-x-0bottom-0 ">
       <div className="text-md text-white">© 2022 <a href="www.thanathip.com" className="hover:underline">Thanathip.Dev</a> All Rights Reserved.
       </div>
 
